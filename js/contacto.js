bf619ae93b7ba1c2630852c08515447736420b76
NazaProsman <34314274+NazaProsman@users.noreply.github.com>
2021-11-25 18:36:06 -0300

    Update contacto.js

--- js/contacto.js
+++ js/contacto.js
@@ -68,3 +68,35 @@ function validar (){
     }else {
         email.classList.add('error')}
 }
+
+/* FILTRAR CALENDARIO */
+
+$(document).ready(function() {
+    $("#filtrar").click(function() {
+        filtrar();
+    })
+})
+function filtrar() {
+    const establecimiento=$("#establecimiento").val();
+    const idioma=$("#idioma").val();
+    
+    $("#cursos article").hide();
+    $(`#cursos .${establecimiento}.${idioma}`).show();  
+}
+
+/* FILTRAR NOMBRE ESTABLECIMIENTO */
+
+$(document).ready(function() {
+    $("#escuelas").hide();
+    $("#filtrarprovincia").click(function() {
+        filtrarprov();
+        
+    })
+})
+function filtrarprov() {
+   
+    const valorestablecimiento=$("#establecimientoboton").val(); 
+   $(`#escuelas .${valorestablecimiento}`).show();  
+   $(`#escuelas `).show();
+
+}
