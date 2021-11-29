146f1e701f9803e005b227fa1aa79793013c56bd
Rayvony <91701599+Rayvony@users.noreply.github.com>
2021-11-28 21:54:58 -0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const elipsis = document.getElementById('elipsis')
-const menu = document.getElementById('menu')
-
-elipsis.addEventListener('click', ()=>{
-    menu.classList.toggle('show')
-    })
